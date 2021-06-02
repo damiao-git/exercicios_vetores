85cbca917fe59b14d7f3e24e996c596b43e5095a
Damião <damiao.jose@hotmail.com>
2021-06-02 14:23:04 -0300

    tentativa 1

---
+++ src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Rent[] rent = new Rent[10];
+		
+		System.out.print("How many rooms will be rented: ");
+		int n = sc.nextInt();
+		System.out.println();
+		
+		for (int i = 1 ; i<=n ; i++) {
+			System.out.println("Rent #" + i);
+			System.out.print("Name: ");
+			String name = sc.next();
+			System.out.print("Email: ");
+			String email = sc.next();
+			System.out.println("Room: ");
+			int room = sc.nextInt();
+			rent[room] = new Rent(name, email, room);
+		}
+		
+		System.out.println();
+		System.out.println("Busy rooms:");
+		
+		for (int i=0;i<=10;i++) {
+			if (rent[i] != null) {
+				System.out.println(rent[i]);
+			}
+		}
+		
+		sc.close();
+	}
+
+}
